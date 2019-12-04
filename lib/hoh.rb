2939e248a0405e6beedff3055af4dbf2fa0fb40f
Tommy Berman <tomy.berman1@gmail.com>
2019-12-04 09:33:23 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -48,11 +48,10 @@ def naming_system
   :sub_category => {
     :label => "Phylum",
     :sub_category => {
-      :label => "Class",{
+      :label => "Class",
       :label => "Order"
       }
     }
   }
-}
 
 end
